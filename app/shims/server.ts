b6e6dd7f2e07a60a3dd734ab2bf7d03c224a78af
bobsabra <65767727+bobsabra@users.noreply.github.com>
2025-08-04 15:34:04 +0300

    Update server.ts

--- app/shims/server.ts
+++ app/shims/server.ts
@@ -1,12 +1,8 @@
-- // server shim (currently pulling browser polyfill)
-- import { ReadableStream, WritableStream, TransformStream } from "stream-browserify/web";
-- export { ReadableStream, WritableStream, TransformStream };
+// Use Node's native Web Streams for SSR
+import {
+  ReadableStream,
+  WritableStream,
+  TransformStream,
+} from "node:stream/web";
 
-+ // Use Node's native Web Streams for SSR
-+ import {
-+   ReadableStream,
-+   WritableStream,
-+   TransformStream,
-+ } from "node:stream/web";
-+
-+ export { ReadableStream, WritableStream, TransformStream };
+export { ReadableStream, WritableStream, TransformStream };
