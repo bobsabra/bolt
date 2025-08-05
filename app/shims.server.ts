c36b3975cff7f1e6aceb9666a765bd88103cba67
bobsabra <65767727+bobsabra@users.noreply.github.com>
2025-08-05 13:23:37 +0300

    Delete app/shims.server.ts

--- app/shims.server.ts
+++
@@ -1,8 +0,0 @@
-// Use Node's native Web Streams for SSR (Remix's shims.server.ts)
-import {
-  ReadableStream,
-  WritableStream,
-  TransformStream,
-} from "node:stream/web";
-
-export { ReadableStream, WritableStream, TransformStream };
